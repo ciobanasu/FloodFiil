892601da03714850cdd0cff266a67a603b939706
Ion Ciobanasu <nikusor.94@gmail.com>
2018-08-02 09:36:22 +0300

    Initial Commit

---
+++ drawTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drawTest
+//
+//  Created by Ciobanasu Ion on 02/08/2018.
+//  Copyright © 2018 Ciobanasu Ion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
